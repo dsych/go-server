44e8461fd490f9188dc22187520396b4d702330f
Dmytro Sych <dmytrosych834@gmail.com>
2018-10-10 17:41:31 -0400

    handling output

--- configure-env.sh
+++ configure-env.sh
@@ -4,14 +4,17 @@ sudo yum update -y nss curl libcurl
 sudo yum install -y git mod_ssl policycoreutils-python
 
 #download go
-wget https://dl.google.com/go/go1.11.1.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.11.1.linux-amd64.tar.gz
-echo 'Add go binary into your path PATH:$PATH:/usr/local/go/bin'
+which go 2> /dev/null
+if [ $? -ne 0 ] then 
+    wget https://dl.google.com/go/go1.11.1.linux-amd64.tar.gz
+    tar -C /usr/local -xzf go1.11.1.linux-amd64.tar.gz
+    echo 'Add go binary into your path PATH:$PATH:/usr/local/go/bin'
+fi
 
 port=1443
 
 echo "Adding exception for port ${port} to iptables"
-iptables --list -n | grep $port
+iptables --list -n | grep $port > /dev/null
 if [ $? -ne 0 ] then
     iptables -I INPUT 2 -p tcp -m state --state NEW -m tcp --dport $port -j ACCEPT
     sudo service iptables save
