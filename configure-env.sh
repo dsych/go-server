c259de28ca27b532eef3a89ae0f2462bd4e7a428
dsych <dmytrosych834@gmail.com>
2018-11-21 01:56:42 -0500

    getting rid of unnecessary port

--- configure-env.sh
+++ configure-env.sh
@@ -15,20 +15,8 @@ fi
 sudo service mysqld start
 sudo chkconfig --level 345 mysqld on
 
-port=1443
 localPort=1444
 
-echo "Adding exception for port ${port} to iptables"
-iptables --list -n | grep $port > /dev/null
-if [ $? -ne 0 ]; then
-    iptables -I INPUT 2 -p tcp -m state --state NEW -m tcp --dport $port -j ACCEPT
-    sudo service iptables save
-    sudo service iptables restart
-else
-    echo "Port ${port} is already present"
-fi
-
 #enable port 1443 through SeLinux
-echo "Adding exception for port ${port} to SeLinux"
-sudo semanage port -a -t http_port_t -p tcp $port
+echo "Adding exception for port ${localPort} to SeLinux"
 sudo semanage port -a -t http_port_t -p tcp $localPort
